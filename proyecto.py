aeae0fac99451d76d54f924b3bedfb20fdebcd33
GerardoRamirez98 <nene19980719@gmail.com>
2022-04-06 16:08:00 -0500

    agregamos clases tranform y load

--- proyecto.py
+++ proyecto.py
@@ -8,6 +8,15 @@ class Extract:
         self.arg_date = '2022-03-02'
         self.src_format = '%Y-%m-%d'
         self.src_bucket = 'deutsche-boerse-xetra-pds'
-        self.trg_bucket = 'xetra-bucket-12345'
+        self.trg_bucket = 'xetra-bucket-2022-03-02-gerardo'
         self.columns = ['ISIN', 'Date', 'Time', 'StartPrice', 'MaxPrice', 'MinPrice', 'EndPrice', 'TradedVolume']
         self.key = 'xetra_daily_report_' + datetime.today().strftime("%Y%m%d_%H%M%S") + '.parquet'
+
+
+class Tranform:
+    def __init__(self):
+        pass
+
+class Load:
+    def __init__(self):
+        pass

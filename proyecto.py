9158cb2a2ef1d928deea4cbc46dd97a8ddacb4a8
GerardoRamirez98 <nene19980719@gmail.com>
2022-04-06 15:57:57 -0500

    proyecto clase extract

--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,13 @@
-class Report:
+import boto3
+import pandas as pd
+from io import StringIO, BytesIO
+from datetime import datetime, timedelta
+
+class Extract:
     def __init__(self):
-        pass
+        self.arg_date = '2022-03-02'
+        self.src_format = '%Y-%m-%d'
+        self.src_bucket = 'deutsche-boerse-xetra-pds'
+        self.trg_bucket = 'xetra-bucket-12345'
+        self.columns = ['ISIN', 'Date', 'Time', 'StartPrice', 'MaxPrice', 'MinPrice', 'EndPrice', 'TradedVolume']
+        self.key = 'xetra_daily_report_' + datetime.today().strftime("%Y%m%d_%H%M%S") + '.parquet'
